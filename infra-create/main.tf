7d1da1f14ab03b406618bc2116661a1dcf5b5055
amrutha-dve <amrutha.irrinki95@gmail.com>
2024-08-04 18:07:53 +0530

    terraform code

--- infra-create/main.tf
+++ infra-create/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "tool" {
     ami         = data.aws_ami.rhel9.image_id
     instance_type = var.instance_type
-    vpc_security_group_ids = [aws_security_group.tools-sg.id]
+    vpc_security_group_ids = [aws_security_group.tool-sg.id]
 
     tags = {
         Name = var.name
