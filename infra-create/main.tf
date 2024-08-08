f16636db100c4e1b3bd6561cffbab58f570a574b
amrutha-dve <amrutha.irrinki95@gmail.com>
2024-08-08 16:30:17 +0530

    changing instance to spot instance

--- infra-create/main.tf
+++ infra-create/main.tf
@@ -39,3 +39,19 @@ resource "aws_security_group" "tool-sg" {
       Name = "${var.name}-sg"
     }
 }
+
+resource "aws_route53" "record-public" {
+    zone_id = var.hosted_zone_id
+    name    = var.name
+    type    =  "A"
+    ttl      = 10
+    records  = [aws_instance.tool.public_ip]
+}
+
+resource "aws_route53" "record-private" {
+    zone_id = var.hosted_zone_id
+    name    = "${var.name}-internal"
+    type    =  "A"
+    ttl      = 10
+    records  = [aws_instance.tool.private_ip]
+}
--- variables.tf
+++ variables.tf
@@ -18,3 +18,6 @@ variable "tools" {
 
     }
 }
+
+variable "hosted_zone_id" {
+    default = "Z0920686IGN3402SVP12"}
